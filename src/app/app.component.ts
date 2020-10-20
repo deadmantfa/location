ba40d02aba04bf3d163b82ae289f5ab5b7867a2f
deadmantfa <wenceslausdsilva@gmail.com>
2020-10-20 18:04:32 +0530

    Location Issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Router} from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {LocationResolverService} from './location-resolver.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'location';
+  links: Array<{ text: string, path: string }> = [];
+
+  constructor(private router: Router) {
+
+    this.router.config.unshift(
+      {
+        path: 'mumbai',
+        resolve: {
+          path: LocationResolverService
+        },
+        component: HomeComponent
+      },
+      {
+        path: 'bengaluru',
+        resolve: {
+          path: LocationResolverService
+        },
+        component: HomeComponent
+      }
+    );
+
+    this.links.push(
+      {text: 'mumbai', path: 'mumbai'},
+      {text: 'bengaluru', path: 'bengaluru'},
+    );
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,52 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {NotfoundComponent} from './notfound/notfound.component';
+import {PathresolverService} from './pathresolver.service';
+import {LocationResolverService} from './location-resolver.service';
+import {ApiService} from './api.service';
+import {HttpClientModule} from '@angular/common/http';
+import {TransferHttpCacheModule} from '@nguniversal/common';
+
+
+const routes: Routes = [
+  {
+    path: '',
+    resolve: {
+      path: LocationResolverService
+    },
+    component: HomeComponent
+  },
+  {
+    path: '**',
+    resolve: {
+      path: PathresolverService
+    },
+    component: NotfoundComponent
+  },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NotfoundComponent
   ],
   imports: [
-    BrowserModule
+    TransferHttpCacheModule,
+    HttpClientModule,
+    BrowserModule.withServerTransition({appId: 'serverApp'}),
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [
+    ApiService,
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
